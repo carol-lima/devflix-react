bcda591ed51a4439496429025222a964adafc89c
Carol <carollima.dev@gmail.com>
2020-08-10 22:44:29 -0300

    Add Carousel

--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -33,7 +33,7 @@ export const ExtraLink = styled.a`
   }
 `;
 
-export const VideoCardList = styled.ul`
+/*export const VideoCardList = styled.ul`
   margin: 0;
   padding-left: 0;
   padding-bottom: 32px;
@@ -45,7 +45,7 @@ export const VideoCardList = styled.ul`
   li {
     margin-right: 16px;
   }
-`;
+`;*/
 
 export const VideoCardGroupContainer = styled.section`
   color: white;
